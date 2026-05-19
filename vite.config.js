bc9b7d92ae094ff8fd0406a46863eef2e2b44ae9
stockAIEngine <waliajdsingh@gmail.com>
2026-05-18 22:32:22 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-
   base: '/ai-swing-trading-tool/',
-
   plugins: [react(), tailwindcss()],
-
 })
